6a73ad44a150d68141ad5abebfd39416f8b59fdb
byiringiroscar <byiringoroscar@gmail.com>
2023-05-19 11:33:33 +0200

    Fix errors

--- decode_morse.rb
+++ decode_morse.rb
@@ -1,26 +1,25 @@
 MORSE_CODE = {
-    ".-" => "A", "-..." => "B", "-.-." => "C", "-.." => "D", "." => "E",
-    "..-." => "F", "--." => "G", "...." => "H", ".." => "I", ".---" => "J",
-    "-.-" => "K", ".-.." => "L", "--" => "M", "-." => "N", "---" => "O",
-    ".--." => "P", "--.-" => "Q", ".-." => "R", "..." => "S", "-" => "T",
-    "..-" => "U", "...-" => "V", ".--" => "W", "-..-" => "X", "-.--" => "Y",
-    "--.." => "Z"
-  }.freeze
-  
-  def decode_char(morse_char)
-    MORSE_CODE[morse_char]
-  end
-  
-  def decode_word(morse_word)
-    morse_word.split(" ").map { |morse_char| decode_char(morse_char) }.join("")
-  end
-  
-  def decode(message)
-    message.split("   ").map { |morse_word| decode_word(morse_word) }.join(" ")
-  end
-  
-  # Example usage
-  message = ".-   -... --- -..-   ..-. ..- .-.. .-..   --- ..-.   .-. ..- -... .. . ..."
-  decoded_message = decode(message)
-  puts decoded_message
-  
+  '.-' => 'A', '-...' => 'B', '-.-.' => 'C', '-..' => 'D', '.' => 'E',
+  '..-.' => 'F', '--.' => 'G', '....' => 'H', '..' => 'I', '.---' => 'J',
+  '-.-' => 'K', '.-..' => 'L', '--' => 'M', '-.' => 'N', '---' => 'O',
+  '.--.' => 'P', '--.-' => 'Q', '.-.' => 'R', '...' => 'S', '-' => 'T',
+  '..-' => 'U', '...-' => 'V', '.--' => 'W', '-..-' => 'X', '-.--' => 'Y',
+  '--..' => 'Z'
+}.freeze
+
+def decode_char(morse_char)
+  MORSE_CODE[morse_char]
+end
+
+def decode_word(morse_word)
+  morse_word.split(' ').map { |morse_char| decode_char(morse_char) }.join('')
+end
+
+def decode(message)
+  message.split('   ').map { |morse_word| decode_word(morse_word) }.join(' ')
+end
+
+
+message = '.-   -... --- -..-   ..-. ..- .-.. .-..   --- ..-.   .-. ..- -... .. . ...'
+decoded_message = decode(message)
+puts decoded_message
