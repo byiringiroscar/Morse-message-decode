db46329afd7d3af37704500b31cedfcdc698058f
byiringiroscar <byiringoroscar@gmail.com>
2023-05-19 11:38:49 +0200

    Fix linters errors

--- decode_morse.rb
+++ decode_morse.rb
@@ -12,13 +12,12 @@ def decode_char(morse_char)
 end
 
 def decode_word(morse_word)
-  morse_word.split(' ').map { |morse_char| decode_char(morse_char) }.join('')
+  morse_word.split.map { |morse_char| decode_char(morse_char) }.join
 end
 
 def decode(message)
   message.split('   ').map { |morse_word| decode_word(morse_word) }.join(' ')
 end
-
 
 message = '.-   -... --- -..-   ..-. ..- .-.. .-..   --- ..-.   .-. ..- -... .. . ...'
 decoded_message = decode(message)
